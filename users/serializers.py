f37a03adb632578660c0714149cb066c6cbe67cf
Islam Murtazaev <islam.muratazaev@gmail.com>
2025-01-18 09:53:25 -0500

    Remove password 2 from registration

--- users/serializers.py
+++ users/serializers.py
@@ -15,11 +15,10 @@ class UserSerializer(serializers.ModelSerializer):
         required=True,
         validators=[validate_password]
     )
-    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 
+        fields = ('username', 'password', 'email', 'first_name', 
                  'last_name', 'phone_number', 'date_of_birth', 
                  'profile_picture')
         extra_kwargs = {
@@ -27,14 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name': {'required': True}
         }
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
-        return attrs
-
     def create(self, validated_data):
-        validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
 
--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='login'),
     path('me/', UserDetailView.as_view(), name='user-detail'),
-] 
+]
